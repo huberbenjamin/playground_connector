22b0356f8fa5286a94aa6fadffbb0de4e69ff977
Azaya Bernard Haposan <azarya.haposan@tum.de>
2026-06-11 16:59:51 +0200

    change run

--- frontend/run.py
+++ frontend/run.py
@@ -4,5 +4,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", 8000))
     app.run(host="0.0.0.0", port=port, debug=True)
